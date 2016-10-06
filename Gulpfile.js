141aee842076dfeb3e2b87f63740e1ce39e4490d
jesperorb <jesper.orb@zocom.se>
2016-10-06 21:03:16 +0200

    Rewamped site

---
+++ Gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('cssnano');
+var babel = require('gulp-babel');
+var minify = require('gulp-minify');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+	return gulp.src('./src/sass/**/*.scss')
+	.pipe(sass().on('error', sass.logError))
+	.pipe(gulp.dest('./dist/css'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('babel', function(){
+	return gulp.src('./src/js/main.js')
+	.pipe(babel())
+	.pipe(gulp.dest('./dist/js'))
+	.pipe(browserSync.stream());
+});
+
+gulp.task('default', ['sass','babel'], function () {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
+	gulp.watch("./*.html").on('change', browserSync.reload);
+	gulp.watch('./src/sass/**/*.scss', ['sass']);
+	gulp.watch('./src/js/**/*.js', ['babel']);
+});
+
+
+gulp.task('css', function () {
+	var processors = [
+	autoprefixer({browsers: ['last 2 version']}),
+	cssnano(),
+	];
+	return gulp.src('./dist/css/*.css')
+	.pipe(postcss(processors))
+	.pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('minify', function(){
+	gulp.src('./dist/js/main.js')
+	.pipe(minify())
+	.pipe(gulp.dest('./dist/js/'));
+});
+
+
---
+++ dist/js/main.js
@@ -0,0 +1,61 @@
+'use strict';
+
+/*jshint esversion: 6 */
+
+// AJAX Call for repos
+$.ajax({
+	method: 'GET',
+	url: 'https://api.github.com/orgs/FEND16/repos?sort=pushed&access_token=658e516551ef662a2c110c9e51d69c8277fdc1a4',
+	dataType: 'JSON',
+	success: function success(result) {
+		appendRepos(sortRepos(result));
+		$(".repoList").fadeOut(0).fadeIn(500);
+	} });
+
+var orderByDate = function orderByDate(arr, dateProp) {
+	return arr.slice().sort(function (a, b) {
+		return a[dateProp] > b[dateProp] ? -1 : 1;
+	});
+};
+
+//Returns list of repos with the neccesary properties: name, link and push-time
+var sortRepos = function sortRepos(data) {
+	var repos = [];
+	data.map(function (repo) {
+		//Convert ISO whatever date string to DateTime-format, usable for sorting later
+		var time_updated = new Date(repo.pushed_at);
+		//Create object for each repo with the three properties, then push all objects to array
+		repos.push({ name: repo.name, link: repo.html_url, updated: time_updated });
+	});
+
+	//Sort by 'updated' to get latest updated repo first in array
+	repos = orderByDate(repos, 'updated');
+	return repos;
+};
+
+var appendRepos = function appendRepos(repos) {
+	var htmlChunk = '';
+	repos.map(function (repo) {
+		htmlChunk += '<ul>\n\t\t\t\t\t\t\t<li><a href=' + repo.link + '><span class="keyword">var </span> ' + repo.name + '</a>;</li>\n\t\t\t\t\t\t\t<li><span class="string">console</span>.<span class="keyword">log</span>(<span class="string">"Uppdaterad: ' + repo.updated.toLocaleString() + '"</span>);</li>\n\t\t\t\t\t\t</ul>';
+	});
+	$('.repoList').append(htmlChunk);
+};
+
+// var appendMembers = function(data){
+// 	let htmlChunk = '';
+// 	data.map((user) =>{
+// 		htmlChunk += `<li><img class='memberImg' src=${user.avatar_url} class='memberImg'/><a href='${user.html_url}'>${user.login}</a>`;
+// 	});
+
+// 	$('.memberList').append(htmlChunk);
+// };
+
+
+// AJAX Call for members of organization
+// $.ajax({
+// 	method: 'GET',
+// 	url: 'https://api.github.com/orgs/FEND16/members?access_token=658e516551ef662a2c110c9e51d69c8277fdc1a4',
+// 	dataType: 'JSON',
+// 	success: function(result){
+// 		appendMembers(result);
+// 	}});
