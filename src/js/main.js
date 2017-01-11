972594918dc0cc0d31aebb9d322f4eeaead71eef
jesperorb <jesper.orb@zocom.se>
2017-01-11 20:01:04 +0100

    This commit is the one before the big ol' clean

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
 /*jshint esversion: 6 */
-var TOKEN = 'f7326de422e386e552172886079eede88e9ffa5c';
 
 // AJAX Call for repos
 $.ajax({
 	method: 'GET',
-	url: 'https://api.github.com/orgs/FEND16/repos?sort=pushed&access_token=' + TOKEN,
+	url: 'https://api.github.com/orgs/FEND16/repos?sort=pushed',
 	dataType: 'JSON',
 	success: function(result){
 		appendRepos(sortRepos(result));
@@ -43,21 +42,3 @@ var appendRepos = function(repos){
 	});
 	$('.repoList').append(htmlChunk);
 };
-
-// var appendMembers = function(data){
-// 	let htmlChunk = '';
-// 	data.map((user) =>{
-// 		htmlChunk += `<li><img class='memberImg' src=${user.avatar_url} class='memberImg'/><a href='${user.html_url}'>${user.login}</a>`;
-// 	});
-
-// 	$('.memberList').append(htmlChunk);
-// };
-
-// AJAX Call for members of organization
-// $.ajax({
-// 	method: 'GET',
-// 	url: 'https://api.github.com/orgs/FEND16/members?access_token=658e516551ef662a2c110c9e51d69c8277fdc1a4',
-// 	dataType: 'JSON',
-// 	success: function(result){
-// 		appendMembers(result);
-// 	}});
