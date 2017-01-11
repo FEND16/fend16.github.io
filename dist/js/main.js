432e62e0843c708c0e98d811dedaf26c0a29187c
jesperorb <jesper.orb@zocom.se>
2017-01-11 20:39:13 +0100

    new site who dis?

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,16 +1,15 @@
 'use strict';
 
 /*jshint esversion: 6 */
-var TOKEN = 'f7326de422e386e552172886079eede88e9ffa5c';
 
 // AJAX Call for repos
 $.ajax({
 	method: 'GET',
-	url: 'https://api.github.com/orgs/FEND16/repos?sort=pushed&access_token=' + TOKEN,
+	url: 'https://api.github.com/orgs/FEND16/repos?sort=pushed',
 	dataType: 'JSON',
 	success: function success(result) {
-		appendRepos(sortRepos(result));
-		$(".repoList").fadeOut(0).fadeIn(500);
+		console.log(sortRepos(result));
+		return sortRepos(result);
 	} });
 
 var orderByDate = function orderByDate(arr, dateProp) {
@@ -21,41 +20,13 @@ var orderByDate = function orderByDate(arr, dateProp) {
 
 //Returns list of repos with the neccesary properties: name, link and push-time
 var sortRepos = function sortRepos(data) {
-	var repos = [];
-	data.map(function (repo) {
-		//Convert ISO whatever date string to DateTime-format, usable for sorting later
-		var time_updated = new Date(repo.pushed_at);
+	var repos = data.map(function (repo) {
 		//Create object for each repo with the three properties, then push all objects to array
-		repos.push({ name: repo.name, link: repo.html_url, updated: time_updated });
+		return { name: repo.name, link: repo.html_url, updated: new Date(repo.pushed_at) };
 	});
 
 	//Sort by 'updated' to get latest updated repo first in array
-	repos = orderByDate(repos, 'updated');
-	return repos;
+	return orderByDate(repos, 'updated');
 };
 
-var appendRepos = function appendRepos(repos) {
-	var htmlChunk = '';
-	repos.map(function (repo) {
-		htmlChunk += '<ul>\n\t\t\t\t\t\t\t<li><a href=' + repo.link + '><span class="keyword">var </span> ' + repo.name + '</a>;</li>\n\t\t\t\t\t\t\t<li><span class="string">console</span>.<span class="keyword">log</span>(<span class="string">"Uppdaterad: ' + repo.updated.toLocaleString() + '"</span>);</li>\n\t\t\t\t\t\t</ul>';
-	});
-	$('.repoList').append(htmlChunk);
-};
-
-// var appendMembers = function(data){
-// 	let htmlChunk = '';
-// 	data.map((user) =>{
-// 		htmlChunk += `<li><img class='memberImg' src=${user.avatar_url} class='memberImg'/><a href='${user.html_url}'>${user.login}</a>`;
-// 	});
-
-// 	$('.memberList').append(htmlChunk);
-// };
-
-// AJAX Call for members of organization
-// $.ajax({
-// 	method: 'GET',
-// 	url: 'https://api.github.com/orgs/FEND16/members?access_token=658e516551ef662a2c110c9e51d69c8277fdc1a4',
-// 	dataType: 'JSON',
-// 	success: function(result){
-// 		appendMembers(result);
-// 	}});
+var appendRepos = function appendRepos(data) {};
--- src/js/main.js
+++ src/js/main.js
@@ -6,39 +6,27 @@ $.ajax({
 	url: 'https://api.github.com/orgs/FEND16/repos?sort=pushed',
 	dataType: 'JSON',
 	success: function(result){
-		appendRepos(sortRepos(result));
-		$(".repoList").fadeOut(0).fadeIn(500);	
+		console.log(sortRepos(result));
+		return sortRepos(result);
 	}});
 
-var orderByDate = function(arr, dateProp) {
-  return arr.slice().sort(function (a, b) {
+var orderByDate = (arr, dateProp) => {
+  return arr.slice().sort((a, b) => {
     return a[dateProp] > b[dateProp] ? -1 : 1;
   });
 };
 
 //Returns list of repos with the neccesary properties: name, link and push-time
-var sortRepos = function(data){
-	let repos = [];
-	data.map((repo) =>{
-		//Convert ISO whatever date string to DateTime-format, usable for sorting later
-		let time_updated = new Date(repo.pushed_at);
+var sortRepos = (data) => {
+	let repos = data.map((repo) => {
 		//Create object for each repo with the three properties, then push all objects to array
-		repos.push({name: repo.name, link: repo.html_url, updated: time_updated});
+		return {name: repo.name, link: repo.html_url, updated: new Date(repo.pushed_at)};
 	});
 
 	//Sort by 'updated' to get latest updated repo first in array
-	repos = orderByDate(repos, 'updated');
-	return repos;
+	return orderByDate(repos, 'updated');
 };
 
-
-var appendRepos = function(repos){
-	let htmlChunk = '';
-	repos.map((repo)=>{
-		htmlChunk += 	`<ul>
-							<li><a href=${repo.link}><span class="keyword">var </span> ${repo.name}</a>;</li>
-							<li><span class="string">console</span>.<span class="keyword">log</span>(<span class="string">"Uppdaterad: ${repo.updated.toLocaleString()}"</span>);</li>
-						</ul>`;
-	});
-	$('.repoList').append(htmlChunk);
-};
+var appendRepos = (data) => {
+	
+}
