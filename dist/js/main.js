001be494a823dcdb4d61430d7f476427ea5db9ee
jesperorb <jesper.orb@zocom.se>
2017-01-13 13:53:51 +0100

    Update index with links The previous version was using jquery, removed jquery and cleaned up the css-file.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,37 +1,54 @@
-'use strict';
+"use strict";
 
 /*jshint esversion: 6 */
+var App = function () {
 
-//AJAX Call for repos
-$.ajax({
-	method: 'GET',
-	url: 'https://api.github.com/orgs/FEND16/repos?sort=pushed',
-	dataType: 'JSON',
-	success: function success(result) {
-		appendRepos(sortRepos(result));
-	} });
+	var getReposFromAPI = function getReposFromAPI() {
+		var req = new XMLHttpRequest();
+		req.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				appendRepos(sortRepos(JSON.parse(req.response)));
+			}
+		};
+		req.open("GET", "https://api.github.com/orgs/FEND16/repos?sort=pushed", true);
+		req.send();
+	};
 
-var orderByDate = function orderByDate(arr, dateProp) {
-	return arr.slice().sort(function (a, b) {
-		return a[dateProp] > b[dateProp] ? -1 : 1;
-	});
-};
+	var orderByDate = function orderByDate(arr, dateProp) {
+		return arr.slice().sort(function (a, b) {
+			return a[dateProp] > b[dateProp] ? -1 : 1;
+		});
+	};
 
-//Returns list of repos with the neccesary properties: name, link and push-time
-var sortRepos = function sortRepos(data) {
-	var repos = data.map(function (repo) {
-		//Create object for each repo with the three properties, then push all objects to array
-		return { name: repo.name, link: repo.html_url, updated: new Date(repo.pushed_at) };
-	});
+	//Returns list of repos with the neccesary properties: name, link and push-time
+	var sortRepos = function sortRepos(data) {
+		var repos = data.map(function (repo) {
+			//Create object for each repo with the three properties, then push all objects to array
+			return { name: repo.name, link: repo.html_url, updated: new Date(repo.pushed_at) };
+		});
 
-	//Sort by 'updated' to get latest updated repo first in array
-	return orderByDate(repos, 'updated');
-};
+		//Sort by 'updated' to get latest updated repo first in array
+		return orderByDate(repos, 'updated');
+	};
 
-var appendRepos = function appendRepos(repos) {
-	var htmlChunk = '';
-	repos.map(function (repo) {
-		htmlChunk += '<div class="repo-link">\n\t\t\t\t\t\t\t<a href="' + repo.link + '"> ' + repo.name + '</a>\n\t\t\t\t\t\t\t<p>' + repo.updated.toLocaleString() + '</p>\n\t\t\t\t\t\t</div>';
-	});
-	$('.repos').append(htmlChunk);
-};
+	var appendRepos = function appendRepos(repos) {
+		var outerDiv = void 0;
+		var repoDiv = document.createElement('div');
+		repoDiv.classList.add('linklist');
+		repoDiv.setAttribute('id', 'repos');
+
+		var htmlChunk = '';
+		repos.map(function (repo) {
+			htmlChunk += "\n\t\t\t\t<a href=\"" + repo.link + "\"> \n\t\t\t\t" + repo.name + " \n\t\t\t\t</a>\n\t\t\t";
+		});
+		repoDiv.innerHTML = htmlChunk;
+		outerDiv = document.getElementById('repos');
+		outerDiv.appendChild(repoDiv);
+	};
+
+	return {
+		getRepos: getReposFromAPI
+	};
+}();
+
+App.getRepos();
--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,57 @@
 /*jshint esversion: 6 */
+const App = (function(){
 
-//AJAX Call for repos
-$.ajax({
-	method: 'GET',
-	url: 'https://api.github.com/orgs/FEND16/repos?sort=pushed',
-	dataType: 'JSON',
-	success: (result) => {
-		appendRepos(sortRepos(result));
-	}});
+	const getReposFromAPI = () => {
+		let req = new XMLHttpRequest();
+		req.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				appendRepos(sortRepos(JSON.parse(req.response)));
+			}
+		};
+		req.open("GET", "https://api.github.com/orgs/FEND16/repos?sort=pushed", true);
+		req.send();
+	};
 
-const orderByDate = (arr, dateProp) => {
-  return arr.slice().sort((a, b) => {
-    return a[dateProp] > b[dateProp] ? -1 : 1;
-  });
-};
+	const orderByDate = (arr, dateProp) => {
+		return arr.slice().sort((a, b) => {
+			return a[dateProp] > b[dateProp] ? -1 : 1;
+		});
+	};
 
-//Returns list of repos with the neccesary properties: name, link and push-time
-const sortRepos = (data) => {
-	let repos = data.map((repo) => {
-		//Create object for each repo with the three properties, then push all objects to array
-		return {name: repo.name, link: repo.html_url, updated: new Date(repo.pushed_at)};
-	});
+	//Returns list of repos with the neccesary properties: name, link and push-time
+	const sortRepos = (data) => {
+		let repos = data.map((repo) => {
+			//Create object for each repo with the three properties, then push all objects to array
+			return {name: repo.name, link: repo.html_url, updated: new Date(repo.pushed_at)};
+		});
 
 	//Sort by 'updated' to get latest updated repo first in array
 	return orderByDate(repos, 'updated');
-};
+	};
 
-const appendRepos = (repos) => {
-	let htmlChunk = '';
-	repos.map((repo) => {
-		htmlChunk += 	`<div class="repo-link">
-							<a href="${repo.link}"> ${repo.name}</a>
-							<p>${repo.updated.toLocaleString()}</p>
-						</div>`;
-	});
-	$('.repos').append(htmlChunk);
-};
+	const appendRepos = (repos) => {
+		let outerDiv;
+		let repoDiv = document.createElement('div');
+		repoDiv.classList.add('linklist');
+		repoDiv.setAttribute('id', 'repos');
+
+		let htmlChunk = '';
+		repos.map((repo) => {
+			htmlChunk += `
+				<a href="${repo.link}"> 
+				${repo.name} 
+				</a>
+			`;
+		});
+		repoDiv.innerHTML = htmlChunk;	
+		outerDiv = document.getElementById('repos');
+		outerDiv.appendChild(repoDiv);
+	};
+
+	return {
+		getRepos: getReposFromAPI
+	};
+})();
+
+
+App.getRepos();
